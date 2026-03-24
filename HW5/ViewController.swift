3e78ed55a84824ebab094838675888d25a193950
SignumGusik <darya.gus@internet.ru>
2026-03-24 16:07:02 +0300

    Initial Commit

---
+++ HW5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW5
+//
+//  Created by Диана on 24/03/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HW5Tests/HW5Tests.swift
@@ -0,0 +1,17 @@
+//
+//  HW5Tests.swift
+//  HW5Tests
+//
+//  Created by Диана on 24/03/2026.
+//
+
+import Testing
+@testable import HW5
+
+struct HW5Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
